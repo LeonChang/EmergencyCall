a41d1e7ba56cc85dc2f2bd6e2837210b2bb72161
LChang <LeonChang08@gmail.com>
2016-07-09 16:12:58 -0400

    Add call button

--- app/src/main/java/com/company/emergencylocator/MainActivity.java
+++ app/src/main/java/com/company/emergencylocator/MainActivity.java
@@ -1,13 +1,46 @@
 package com.company.emergencylocator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "Emergency Locator Main Activity";
+    private Button mCallButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCallButton = (Button) findViewById(R.id.callButton);
+
+        mCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeCall(getEmergencyNumber());
+            }
+        });
+
+    }
+
+    private String getEmergencyNumber() {
+        return "9083926510";
+    }
+
+    private void makeCall(String number) {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+number));
+        try{
+            startActivity(intent);
+        }
+        catch (Exception e) {
+            Log.i(TAG, "The makeCall() method didn't work, with exception: " + e);
+        }
     }
 }
